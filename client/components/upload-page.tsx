faa89b820a34eb5b7c37593342facae888463afe
sebcio <103338510+sebcioxd@users.noreply.github.com>
2025-05-17 19:43:16 +0200

    lets see

--- client/components/upload-page.tsx
+++ client/components/upload-page.tsx
@@ -256,7 +256,10 @@ export function UploadPage() {
                   d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                 ></path>
               </svg>
-              {uploadProgress > 0 ? `Wysyłanie... ${uploadProgress}%` : 'Przetwarzanie...'}
+              {uploadProgress === 100 
+                ? 'Przetwarzanie plików...' 
+                : `Wysyłanie... ${uploadProgress}%`
+              }
             </span>
           ) : (
             <span className="flex items-center justify-center">
@@ -275,7 +278,10 @@ export function UploadPage() {
               />
             </div>
             <p className="text-xs text-zinc-400 mt-1 text-center">
-              {uploadProgress}% ukończono
+              {uploadProgress === 100 
+                ? 'Przetwarzanie plików...' 
+                : `${uploadProgress}% ukończono`
+              }
             </p>
           </div>
         )}
