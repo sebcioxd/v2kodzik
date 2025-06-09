1702ab680f637475313a0766c9f945b00a81026f
niarde <elak.sebcio@gmail.com>
2025-06-09 13:14:53 +0200

    fix fe: footer text

--- client/components/footer.tsx
+++ client/components/footer.tsx
@@ -5,8 +5,8 @@ export default function Footer() {
     return (
         <footer className="flex flex-col justify-center items-center my-20">
             <CheckStatus />
-            <p className="text-zinc-700 text-xs">wykonanie - <Link href="https://niarde.xyz" className="text-zinc-500 hover:text-zinc-100">niarde.xyz</Link>. pwr by <Link href="https://kaczynskiweb.vercel.app/" className="text-zinc-500 hover:text-zinc-100">kaczynski company</Link></p>
-            <p className="text-zinc-700 text-xs">
+            <p className="text-zinc-700 text-sm">wykonanie - <Link href="https://niarde.xyz" className="text-zinc-500 hover:text-zinc-100">niarde.xyz</Link>. pwr by <Link href="https://kaczynskiweb.vercel.app/" className="text-zinc-500 hover:text-zinc-100">kaczynski company</Link></p>
+            <p className="text-zinc-700 text-sm">
                 <Link href="/terms" className="text-zinc-500 hover:text-zinc-100 mr-2">regulamin</Link>
                 <Link href="https://github.com/sebcioxd/v2kodzik" className="text-zinc-500 hover:text-zinc-100">kod źródłowy</Link>
             </p>
