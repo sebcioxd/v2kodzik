6279b9498954312dd2c1fe69611406f306131d83
sebcio <103338510+sebcioxd@users.noreply.github.com>
2025-07-15 12:57:20 +0200

    feat: mailgun

--- server/src/services/email.service.ts
+++ server/src/services/email.service.ts
@@ -1,25 +1,20 @@
 import type { EmailServiceProps } from '../lib/types'
 import { emailVerifyTemplate } from "../templates/email-verify"
 import { passwordForgetTemplate } from "../templates/password-forget"
-import { SMTP_USER, SMTP_PASS } from '../lib/env'
+import { SMTP_USER, SMTP_PASS, MAILGUN_API_KEY } from '../lib/env'
 import { createMessage } from "@upyo/core";
-import { SmtpTransport } from "@upyo/smtp";
-
+import { MailgunTransport } from "@upyo/mailgun";
 export async function sendEmailService({ to, subject, text, emailType }: EmailServiceProps) {
     const emailTemplate = emailType === "verify" ? emailVerifyTemplate(text, to) : passwordForgetTemplate(text)
 
-    const transporter = new SmtpTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true, 
-        auth: {
-            user: SMTP_USER,
-            pass: SMTP_PASS
-        }
+    const transporter = new MailgunTransport({
+        apiKey: MAILGUN_API_KEY,
+        domain: "mail.dajkodzik.pl",
+        region: "eu"
     })
 
     const message = createMessage({
-        from: SMTP_USER,
+        from: "hello@dajkodzik.pl",
         to: to,
         subject: subject,
         content: { html: emailTemplate || "" }
