c50ccd200f119ec81d1bfc4e29040b8790922cc3
sebcio <103338510+sebcioxd@users.noreply.github.com>
2025-07-16 15:51:39 +0200

    feat: class test

--- server/src/routes/download.route.ts
+++ server/src/routes/download.route.ts
@@ -1,14 +1,16 @@
 import { Hono } from "hono"
-import { downloadFileService, downloadBulkFilesService } from "../services/download.service"
+import { DownloadService } from "../services/download.service"
 import { createRateLimiter } from "../services/rate-limit.service"
+
 const downloadRoute = new Hono()
+const downloadService = new DownloadService("sharesbucket")
 
 downloadRoute.get("/:folder/:file", createRateLimiter("download"), async (c) => {
     const folder = c.req.param("folder")
     const fileName = c.req.param("file")
     const path = `${folder}/${fileName}`
 
-    return await downloadFileService({
+    return await downloadService.downloadFile({
         path,
         c
     })
@@ -17,7 +19,7 @@ downloadRoute.get("/:folder/:file", createRateLimiter("download"), async (c) => 
 downloadRoute.post("/bulk", createRateLimiter("download"), async (c) => {
     try {
         const { paths } = await c.req.json()
-        return await downloadBulkFilesService({ paths, c })
+        return await downloadService.downloadBulkFiles({ paths, c })
     } catch (err) {
         return c.json({
             message: "Wystąpił błąd podczas pobierania plików",
