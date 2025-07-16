14722d2827fc66f26bc90f8881448be60721ddec
sebcio <103338510+sebcioxd@users.noreply.github.com>
2025-07-16 16:15:39 +0200

    feat: add classes to upload

--- server/src/routes/upload.route.ts
+++ server/src/routes/upload.route.ts
@@ -1,28 +1,26 @@
 import type { AuthSession } from "../lib/types";
 import type { Context } from "hono";
-import { finalizeUploadService, S3UploadService, cancelUploadService } from "../services/upload.service";
+import { UploadService } from "../services/upload.service";
 import { createRateLimiter } from "../services/rate-limit.service";
 import { Hono } from "hono";
 
 const uploadRoute = new Hono<AuthSession>();
+const uploadService = new UploadService("sharesbucket");
 
 uploadRoute.post("/presign", createRateLimiter("upload"), async (c: Context) => {
     const user = c.get("user");
-
-    return await S3UploadService({ c, user });
-   
+    return await uploadService.uploadFiles({ c, user });
 });
-
 
 uploadRoute.post("/finalize", async (c: Context) => {
     const user = c.get("user");
-    return await finalizeUploadService({ c, user });
+    return await uploadService.finalizeUpload({ c, user });
 });
 
 uploadRoute.get("/cancel/:slug", async (c: Context) => {
     const { slug } = c.req.param();
 
-    return await cancelUploadService({ c, slug });
+    return await uploadService.cancelUpload({ c, slug });
 });
 
 export default uploadRoute;
--- server/src/services/upload.service.ts
+++ server/src/services/upload.service.ts
@@ -8,162 +8,139 @@ import { getS3Client } from "../lib/s3";
 import { verifyCaptcha } from "../lib/captcha";
 import { S3Client } from "bun";
 
-// Klasa, która zarządza pojedynczym klientem S3.
-// Jest to zrobione w celu uniknięcia tworzenia nowego klienta S3 za każdym razem, gdy jest to potrzebne.
-class S3 {
-    private static client: S3Client;
+export class UploadService {
+    private client: S3Client;
+    
+    constructor(bucket: string) {
+        this.client = getS3Client({ bucket });
+    }
 
-    static getClient() {
-        if (!this.client) {
-            this.client = getS3Client({ bucket: "sharesbucket" });
+    private generatePresignedUrl(key: string) {
+        if (!key || typeof key !== 'string') {
+            throw new Error('Invalid Key provided');
         }
-        return this.client;
-    }
-}
 
+        if (key.includes('..') || key.startsWith('/')) {
+            throw new Error('Invalid file path');
+        }
 
-// Funkcja, która generuje presigned URL dla plików do wysyłki.
-export async function generatePresignedUrl({ Key }: generatePresignedUrlProps) {
-    // Zabezpieczenia generowania url:
-    const client = S3.getClient();
-    if (!Key || typeof Key !== 'string') {
-        throw new Error('Invalid Key provided');
-    }
+        const url = this.client.presign(key, {
+            expiresIn: 600,
+            method: "PUT",
+            type: "application/octet-stream",
+            acl: "public-read",
+        });
 
-    if (Key.includes('..') || Key.startsWith('/')) {
-        throw new Error('Invalid file path');
+        return { url };
     }
 
-    const url = client.presign(Key, {
-        expiresIn: 600,
-        method: "PUT",
-        type: "application/octet-stream",
-        acl: "public-read",
-    })
-
-    return { url }
-}
-
-// Funkcja, która generuje presigned URL dla plików do wysyłki, oraz zwraca wstępne dane do udostępnionego linku.
-// Wszystkie dane są pobierane z requestu, oraz są walidowane.
-// Nie jest to ostateczna funkcja, która tworzy udostępniony link, ale tylko generuje presigned URL dla plików do wysyłki.
-   
-export async function S3UploadService({ c, user }: S3UploadServiceProps) {
-    try {
-        const { slug, isPrivate, accessCode, visibility, time } = c.req.query();
-        const fileNames = c.req.query("fileNames")?.split(",");
-
-        // Walidacja czy człowiek nie jest robotem
+    public async uploadFiles({ c, user }: S3UploadServiceProps) {
         try {
-            await verifyCaptcha({ c });
-        } catch (error) {
+            const { slug, isPrivate, accessCode, visibility, time } = c.req.query();
+            const fileNames = c.req.query("fileNames")?.split(",");
+
+            try {
+                await verifyCaptcha({ c });
+            } catch (error) {
+                return c.json({
+                    message: `Nie udało się zweryfikować captchy. ${error}`,
+                }, 400);
+            }
+
+            const result = await fixRequestProps({ slug, isPrivate, accessCode, visibility, time, fileNames: fileNames || [] }, c, user);
+
+            if (result instanceof Response) {
+                return result;
+            }
+
+            const req: UploadRequestProps = result;
+
+            const presignedData = await Promise.all(req.fileNames.map(async (fileName) => {
+                const uploadService = this.generatePresignedUrl(`${req.slug}/${fileName}`);
+                return {
+                    fileName,
+                    ...uploadService
+                };
+            }));
+
             return c.json({
-                message: `Nie udało się zweryfikować captchy. ${error}`,
-            }, 400);
-        }
-
-
-        const result = await fixRequestProps({ slug, isPrivate, accessCode, visibility, time, fileNames: fileNames || [] }, c, user);
-
-        if (result instanceof Response) {
-            return result;
-        }
-
-        const req: UploadRequestProps = result;
-
-        const presignedData = await Promise.all(req.fileNames.map(async (fileName) => {
-            const uploadService = await generatePresignedUrl({
-                Key: `${req.slug}/${fileName}`,
+                presignedData,
+                slug: req.slug,
+                time: req.time
             });
 
-            return {
-                fileName,
-                ...uploadService
-            };
-        }));
-
-        return c.json({
-            presignedData,
-            slug: req.slug,
-            time: req.time
-        });
-
-    } catch (error) {
-        return c.json({
-            message: "Błąd podczas generowania presigned URL",
-            error
-        }, 500);
-    }   
-}
-
-
-export async function cancelUploadService({ c }: CancelUploadServiceProps) {
-    const { slug } = c.req.query();
-
-    const checkForUploadedFiles = await db
-        .select()
-        .from(uploadedFiles)
-        .where(sql`${uploadedFiles.storagePath} LIKE ${slug + '/%'}`);
-
-    // If length > 0, files exist for this slug
-    const hasFiles = checkForUploadedFiles.length > 0;
-
-    if (hasFiles) {
-        return c.json({
-            message: "Pliki zostały już wysłane",
-        }, 400);
+        } catch (error) {
+            return c.json({
+                message: "Błąd podczas generowania presigned URL",
+                error
+            }, 500);
+        }   
     }
-}
 
-// Funkcja, która finalizuje wysyłanie plików do udostępnionego linku.
-// Wszystkie dane są pobierane z requestu, oraz są zapisywane do bazy danych. (Takie jak informacje o plikach, oraz informacje o udostępnionym linku)
+    public async cancelUpload({ c }: CancelUploadServiceProps) {
+        const { slug } = c.req.query();
 
-export async function finalizeUploadService({ c, user }: FinalizeUploadServiceProps) {
-    const { 
-        slug,
-        files,
-        isPrivate,
-        accessCode,
-        visibility,
-        time 
-    } = await c.req.json();
+        const checkForUploadedFiles = await db
+            .select()
+            .from(uploadedFiles)
+            .where(sql`${uploadedFiles.storagePath} LIKE ${slug + '/%'}`);
 
-    try {
-        const result = await db.transaction(async (tx) => {
-            const shareResult = await tx.insert(shares).values({
-                slug: slug,
-                createdAt: new Date(),
-                updatedAt: new Date(),
-                expiresAt: new Date(Date.now() + (time === "24" ? 24 * 60 * 60 * 1000 : time === "168" ? 7 * 24 * 60 * 60 * 1000 : 30 * 60 * 1000)), 
-                userId: user ? user.id : null,
-                private: isPrivate,
-                code: accessCode ? await hashCode(accessCode) : null,
-                visibility: visibility,
-                ipAddress: c.req.header("x-forwarded-for") || null,
-                userAgent: c.req.header("user-agent") || null,
-            }).returning({ id: shares.id });
+        const hasFiles = checkForUploadedFiles.length > 0;
 
-            const fileInserts = files.map((file: { fileName: string; size: number }) => ({
-                shareId: shareResult[0].id,
-                fileName: file.fileName,
-                size: file.size,
-                storagePath: `${slug}/${file.fileName}`,
-            }));
-            
-            await tx.insert(uploadedFiles).values(fileInserts);
+        if (hasFiles) {
+            return c.json({
+                message: "Pliki zostały już wysłane",
+            }, 400);
+        }
+    }
 
-            return shareResult[0].id;
-        });
+    public async finalizeUpload({ c, user }: FinalizeUploadServiceProps) {
+        const { 
+            slug,
+            files,
+            isPrivate,
+            accessCode,
+            visibility,
+            time 
+        } = await c.req.json();
 
-        return c.json({
-            message: "Pliki zostały wysłane pomyślnie",
-            shareId: result
-        });
+        try {
+            const result = await db.transaction(async (tx) => {
+                const shareResult = await tx.insert(shares).values({
+                    slug: slug,
+                    createdAt: new Date(),
+                    updatedAt: new Date(),
+                    expiresAt: new Date(Date.now() + (time === "24" ? 24 * 60 * 60 * 1000 : time === "168" ? 7 * 24 * 60 * 60 * 1000 : 30 * 60 * 1000)), 
+                    userId: user ? user.id : null,
+                    private: isPrivate,
+                    code: accessCode ? await hashCode(accessCode) : null,
+                    visibility: visibility,
+                    ipAddress: c.req.header("x-forwarded-for") || null,
+                    userAgent: c.req.header("user-agent") || null,
+                }).returning({ id: shares.id });
 
-    } catch (error) {
-        return c.json({
-            message: "Wystąpił błąd podczas wysyłania plików",
-            error
-        }, 500);
+                const fileInserts = files.map((file: { fileName: string; size: number }) => ({
+                    shareId: shareResult[0].id,
+                    fileName: file.fileName,
+                    size: file.size,
+                    storagePath: `${slug}/${file.fileName}`,
+                }));
+                
+                await tx.insert(uploadedFiles).values(fileInserts);
+
+                return shareResult[0].id;
+            });
+
+            return c.json({
+                message: "Pliki zostały wysłane pomyślnie",
+                shareId: result
+            });
+
+        } catch (error) {
+            return c.json({
+                message: "Wystąpił błąd podczas wysyłania plików",
+                error
+            }, 500);
+        }
     }
 }
