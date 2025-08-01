607c8da6140dfd3145535884d4916cd5d8a97dba
sebcio <103338510+sebcioxd@users.noreply.github.com>
2025-08-01 15:53:46 +0200

    feat: fix the slug skeleton

--- client/app/[slug]/loading.tsx
+++ client/app/[slug]/loading.tsx
@@ -8,7 +8,7 @@ export default function Loading() {
         <div className="flex flex-col gap-2">
           <div className="border-b border-dashed border-zinc-800 p-3 bg-zinc-950/10 backdrop-blur-sm flex items-center justify-between">
             <Skeleton className="h-4 w-40 bg-zinc-800" />
-            <Skeleton className="h-8 w-8 bg-zinc-800 rounded-md" />
+            <Skeleton className="h-4 w-8 bg-zinc-800 rounded-md" />
           </div>
         </div>
 
@@ -19,12 +19,12 @@ export default function Loading() {
         </div>
 
         {/* Bulk download button */}
-        <Skeleton className="h-5 w-full bg-zinc-800 rounded-md" />
+        <Skeleton className="h-4 w-full bg-zinc-800 rounded-md" />
 
         {/* Files count and total size */}
         <div className="flex justify-between items-center border-b border-dashed border-zinc-800 pb-2">
-          <Skeleton className="h-4 w-20 bg-zinc-800" />
-          <Skeleton className="h-4 w-24 bg-zinc-800" />
+          <Skeleton className="h-3 w-20 bg-zinc-800" />
+          <Skeleton className="h-3   w-24 bg-zinc-800" />
         </div>
 
         {/* File items */}
@@ -35,15 +35,15 @@ export default function Loading() {
           >
             <div className="flex items-center justify-between">
               <div className="flex items-center space-x-3">
-                <Skeleton className="h-5 w-5 bg-zinc-800" />
-                <div className="flex flex-col gap-1">
-                  <Skeleton className="h-4 w-48 bg-zinc-800" />
+                <Skeleton className="h-4 w-4 bg-zinc-800" />
+                <div className="flex flex-col gap-2">
+                  <Skeleton className="h-3 w-48 bg-zinc-800" />
                   <Skeleton className="h-3 w-16 bg-zinc-800" />
                 </div>
               </div>
               <div className="flex items-center space-x-1">
-                <Skeleton className="h-6 w-6 bg-zinc-800 rounded-md" />
-                <Skeleton className="h-6 w-6 bg-zinc-800 rounded-md" />
+                <Skeleton className="h-5 w-5 bg-zinc-800 rounded-md" />
+                <Skeleton className="h-5 w-5 bg-zinc-800 rounded-md" />
               </div>
             </div>
           </div>
