1febb2a5dfef9f11a520eb7ab452cc4ce14fd584
sebcio <103338510+sebcioxd@users.noreply.github.com>
2025-09-27 00:04:40 +0200

    fixes

--- client/app/auth/oauth/page.tsx
+++ client/app/auth/oauth/page.tsx
@@ -1,9 +1,20 @@
+import { Suspense } from "react";
 import OAuth from "@/components/auth/oauth";
 
 export default function OAuthPage() {
     return (
-        <div>
+        <Suspense fallback={
+            <div className="md:my-30 my-10 flex items-center justify-center bg-darken">
+                <div className="text-center space-y-4">
+                    <div className="h-8 w-8 animate-spin mx-auto text-zinc-400 border-2 border-zinc-400 border-t-transparent rounded-full" />
+                    <h1 className="text-xl font-medium text-zinc-200">
+                        Ładowanie...
+                    </h1>
+                    <p className="text-zinc-400">Prosimy o chwilkę...</p>
+                </div>
+            </div>
+        }>
             <OAuth />
-        </div>
+        </Suspense>
     )
 }
--- client/app/auth/otp/page.tsx
+++ client/app/auth/otp/page.tsx
@@ -1,9 +1,20 @@
 import OTPInput from "@/components/auth/otp"
+import { Suspense } from "react";
 
 export default function OtpPage() {
     return (
-        <div className="flex flex-col items-center justify-center container mx-auto lg:px-40 md:px-30 px-10 mt-5">
+        <Suspense fallback={
+            <div className="flex flex-col items-center justify-center container mx-auto lg:px-40 md:px-30 px-10 mt-5">
+                <div className="text-center space-y-4">
+                    <div className="h-8 w-8 animate-spin mx-auto text-zinc-400 border-2 border-zinc-400 border-t-transparent rounded-full" />
+                    <h1 className="text-xl font-medium text-zinc-200">
+                        Ładowanie...
+                    </h1>
+                    <p className="text-zinc-400">Prosimy o chwilkę...</p>
+                </div>
+            </div>
+        }>
             <OTPInput />
-        </div>
+        </Suspense>
     )
 }
