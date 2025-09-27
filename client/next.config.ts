0a02338baf1db283841de0fa6a44a3eaee1bed9f
sebcio <103338510+sebcioxd@users.noreply.github.com>
2025-09-27 22:16:46 +0200

    feat: fix front-end

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,13 +4,6 @@ const nextConfig: NextConfig = {
   experimental: {
     browserDebugInfoInTerminal: true,
   },
-  webpack: (config, { webpack, buildId, isServer }) => {
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.NEXT_PUBLIC_CONFIG_BUILD_ID': JSON.stringify(buildId)
-      })
-    );
-}
 };
 
 
