861bf764ba2490240de6a12fdd2ff3d26bbe72e5
sebcio <103338510+sebcioxd@users.noreply.github.com>
2025-11-27 21:55:54 +0100

    feat: front-end

--- client/components/footer.tsx
+++ client/components/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="flex flex-col justify-center items-center my-20">
+        <footer className="flex flex-col justify-center items-center my-20 animate-slide-in-bottom">
             <CheckStatus />
             <p className="text-zinc-700 text-sm">wykonanie - <Link href="https://niarde.xyz" className="text-zinc-500 hover:text-zinc-100">niarde.xyz</Link>. pwr by <Link href="https://kaczynskiweb.vercel.app/" className="text-zinc-500 hover:text-zinc-100">kaczynski company</Link></p>
             <p className="text-zinc-700 text-sm">
